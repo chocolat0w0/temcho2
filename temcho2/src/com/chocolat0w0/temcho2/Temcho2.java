5d4e25a45c1cc8d5be7e8ead2d8a45669bdb9608
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2013-07-28 15:25:43 +0900

    create Project

---
+++ temcho2/src/com/chocolat0w0/temcho2/Temcho2.java
@@ -0,0 +1,12 @@
+package com.chocolat0w0.temcho2;
+
+import javax.swing.JFrame;
+
+public class Temcho2 {
+	public static void main(String arg[]) {
+		JFrame frame = new JFrame("タイトル");
+		frame.setBounds(100, 100, 200, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
