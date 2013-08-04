dd03ad1bed67618355d1da6b807cf4d795411632
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2013-08-04 23:13:04 +0900

    startボタンでタイマーをスタート

--- temcho2/src/com/chocolat0w0/temcho2/MainView.java
+++ temcho2/src/com/chocolat0w0/temcho2/MainView.java
@@ -2,13 +2,21 @@ package com.chocolat0w0.temcho2;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.Console;
+import java.sql.Time;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class MainView {
+public class MainView implements OnTickListener {
+	private OnClickListener mListener;
+	private JLabel mLabelTimer;
+
 	public MainView() {
 		JFrame frame = new JFrame("タイトル");
 		frame.setBounds(100, 100, 200, 200);
@@ -16,12 +24,68 @@ public class MainView {
 		
 		JPanel panel = new JPanel();
 		
-		JLabel labelTimer = new JLabel("00:00");
-		panel.add(labelTimer);
+		mLabelTimer = new JLabel("00:00");
+		panel.add(mLabelTimer);
+		JButton startBtn = new JButton("start");
+		startBtn.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				mListener.onClick();
+			}
+		});
+		panel.add(startBtn);
+		
 		panel.setVisible(true);
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		
 		frame.setVisible(true);
-		
 	}
+	
+	void setOnClickListener(OnClickListener listener) {
+		this.mListener = listener;
+	}
+
+	@Override
+	public void onTick(Time time) {
+		mLabelTimer.setText(formatTime(time));
+	}
+	
+	private String formatTime(Time time) {
+		long sec = time.getTime() / 1000;
+		
+		return formatZero(sec / 60) + ":" + formatZero(sec % 60);
+	}
+	
+	private String formatZero(long orig) {
+		if (orig < 10) {
+			return "0" + Long.toString(orig);
+		}
+		return Long.toString(orig);
+	}
+
 }
--- temcho2/src/com/chocolat0w0/temcho2/TimerController.java
+++ temcho2/src/com/chocolat0w0/temcho2/TimerController.java
@@ -1,20 +1,33 @@
 package com.chocolat0w0.temcho2;
 
 import java.sql.Time;
+import java.util.Timer;
 import java.util.TimerTask;
 
-public class TimerController extends TimerTask {
+public class TimerController extends TimerTask implements OnClickListener {
 	Time mTime = null;
+	Timer mTimer = null;
+	private OnTickListener mListener;
 	
-	TimerController() {
-		mTime = new Time(0);
+	TimerController(Timer timer) {
+		this.mTime = new Time(0);
+		this.mTimer = timer;
 	}
 	
 	@Override
 	public void run() {
 		mTime.setTime(mTime.getTime() + 1000);
 		java.lang.System.out.println("time:" + mTime.getTime());
-		
+		mListener.onTick(mTime);
+	}
+
+	@Override
+	public void onClick() {
+		mTimer.schedule(this, 0, 1000);
+	}
+	
+	void setOnTickListener(OnTickListener listener) {
+		this.mListener = listener;
 	}
 
 }
