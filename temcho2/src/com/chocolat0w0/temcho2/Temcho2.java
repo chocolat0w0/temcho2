662e5f5ae87724275e80a7c03adcae398bb9f209
chocolat0w0 <shiori.yamazaki.1002@gmail.com>
2013-08-28 05:35:37 +0900

    クラス図追加、テキストボックスを修正

--- temcho2/src/com/chocolat0w0/temcho2/Temcho2.java
+++ temcho2/src/com/chocolat0w0/temcho2/Temcho2.java
@@ -1,12 +1,10 @@
 package com.chocolat0w0.temcho2;
 
-import java.util.Timer;
 
 public class Temcho2 {
 	
 	public static void main(String arg[]) {
-		Timer timer = new Timer(true);
-		TimerController timerController = new TimerController(timer);
+		TimerController timerController = new TimerController();
 		
 		MainView mainView = new MainView();
 		mainView.setOnClickListener(timerController);
